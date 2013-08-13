d338e11d926f178ba817ba6f40c8a29c3cd5b6be
Benjamin PIERRE <bp@kapt.mobi>
2013-08-13 10:23:04 +0200

    Fix the setup

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     author='Kapt',
     author_email='dev@kapt.mobi',
     package_dir={'': 'src'},
+    packages=find_packages("src",),
     include_package_data=True,
     description='A django field providing nationalities',
     long_description=open('README.rst').read(),
     zip_safe=False,
     install_requires=[
         "Django==1.5.1",
+        "South==0.7.6",
     ],
 )
