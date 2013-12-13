709273aff46e6e094cc772822226c2dd8b41d8fb
Adrien Delhorme <ad@kapt.mobi>
2013-12-13 16:30:57 +0100

    Remove src directory in order to work with pip.

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     version='0.1',
     author='Kapt',
     author_email='dev@kapt.mobi',
-    package_dir={'': 'src'},
-    packages=find_packages("src",),
+    packages=find_packages(),
     include_package_data=True,
     description='A django field providing nationalities',
     long_description=open('README.rst').read(),
     zip_safe=False,
     install_requires=[
-        "Django==1.5.1",
+        "Django>=1.5",
         "South==0.7.6",
     ],
 )
